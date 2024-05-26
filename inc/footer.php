27ccb6058651859fd89b735156de33868de8a241
eduardomartinez1977 <147462363+eduardomartinez1977@users.noreply.github.com>
2024-05-26 15:24:54 +0000

    Pending changes exported from your codespace

---
+++ inc/footer.php
@@ -0,0 +1,44 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contactanos</h4>
+                <a href="#" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; Twitter </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Porque elegirnos</h4>
+                <p> 
+                    Queremos llevarte lo que buscas.</br>
+                    al mejor costo.</br>
+                   
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Direccion</h4>
+                <p style="color: #FFF">Bogota Colombia</p>
+                <p style="color: #FFF">Direccion Fisica</p>
+                <p style="font-size:20px" aria-hidden="true">  Cra 90 # 83 36 </p>
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true"> 3502517388</p> <a href="#" target="_blank" style="color: #5bc0de">envianos_un_mensaje</a></br>
+                <a href="mailto:zabdiel.martinez@gmail.com?Subject=Interesado%20en%20el%20curso">Contactar por correo</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">Tienda ACAINGSOFT 2024</h5>
+</footer>
